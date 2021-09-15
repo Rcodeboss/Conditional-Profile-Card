a606bc21bcf0fe3bc563855a15d388ce715215a8
Rcodeboss <morejonrafa@gmail.com>
2021-09-15 23:01:36 +0000

    adding conditional profile card

--- src/js/index.js
+++ src/js/index.js
@@ -33,13 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <h3>${variables.friends}, ${variables.laptop}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://twitter.com/Rafa_MJnnnn"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/Rcodeboss"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/rafael-morejon-85533a150/"><i class="fa fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
@@ -54,9 +55,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://www.adobe.com/express/create/media_1bcd514348a568faed99e65f5249895e38b06c947.jpeg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
@@ -68,7 +71,9 @@ window.onload = function() {
     lastname: null,
     role: null,
     country: null,
-    city: null
+    city: null,
+    friends: null,
+    laptop: null
   };
   render(window.variables); //render the card for the first time
 
